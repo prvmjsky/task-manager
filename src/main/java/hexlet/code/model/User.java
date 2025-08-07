ae69eba0c46ff87af28772d81ea34d719fef7f48
prvmjsky <lukyanovalexey1@gmail.com>
2025-08-07 18:03:51 +0500

    lint

--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
+
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
