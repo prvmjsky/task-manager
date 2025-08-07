eaa4c0879f8fdd3d775b2f86eb9ea8df2bb0440d
prvmjsky <lukyanovalexey1@gmail.com>
2025-08-07 23:39:06 +0500

    add show for users + ResourceNotFoundException exception with handler

--- src/main/java/hexlet/code/controller/api/UsersController.java
+++ src/main/java/hexlet/code/controller/api/UsersController.java
@@ -1,10 +1,12 @@
 package hexlet.code.controller.api;
 
 import hexlet.code.dto.UserDTO;
+import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.mapper.UserMapper;
 import hexlet.code.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,14 @@ public class UsersController {
         return repository.findAll().stream()
             .map(mapper::map)
             .toList();
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO show(@PathVariable Long id) {
+
+        var model = repository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException(String.format("User with id %d not found", id)));
+
+        return mapper.map(model);
     }
 }
