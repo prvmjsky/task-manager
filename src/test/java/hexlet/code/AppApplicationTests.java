0f2121383e4ecad55b766da127f91656e09a0714
prvmjsky <lukyanovalexey1@gmail.com>
2025-08-09 01:56:27 +0500

    try to create DataInitializer

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -1,13 +1,21 @@
 package hexlet.code;
 
+import hexlet.code.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class AppApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
-    void contextLoads() {
+    void testDataInitialization() {
+        assertTrue(userRepository.findByEmail("hexlet@example.com").isPresent());
     }
 
 }
