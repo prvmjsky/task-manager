7875a48e2bc53701b2638c869bf43d7c0be6997b
prvmjsky <lukyanovalexey1@gmail.com>
2025-08-14 16:47:39 +0500

    add default statuses to DataInitializer

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -1,6 +1,8 @@
 package hexlet.code.component;
 
 import hexlet.code.dto.user.UserCreateDTO;
+import hexlet.code.model.TaskStatus;
+import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -9,18 +11,38 @@ import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
 
+import java.util.Map;
+
 @Component
 @AllArgsConstructor
 public class DataInitializer implements ApplicationRunner {
 
     @Autowired
-    private final UserService userService;
+    private UserService userService;
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
         var user = new UserCreateDTO();
         user.setEmail("hexlet@example.com");
         user.setPassword("qwerty");
         userService.create(user);
+
+        var defaultStatuses = Map.of(
+            "Draft", "draft",
+            "ToReview", "to_review",
+            "ToBeFixed", "to_be_fixed",
+            "ToPublish", "to_publish",
+            "Published", "published");
+
+        defaultStatuses.forEach((name, slug) -> {
+            var status = new TaskStatus();
+            status.setName(name);
+            status.setSlug(slug);
+            taskStatusRepository.save(status);
+        });
     }
 }
