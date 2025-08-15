be864d0f64b3f6aafce900af090f9207b8396d35
prvmjsky <lukyanovalexey1@gmail.com>
2025-08-16 03:41:49 +0500

    create TaskMapper and TaskRepository

---
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -0,0 +1,68 @@
+package hexlet.code.mapper;
+
+import hexlet.code.dto.task.TaskCreateDTO;
+import hexlet.code.dto.task.TaskDTO;
+import hexlet.code.dto.task.TaskUpdateDTO;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.model.Task;
+import hexlet.code.model.TaskStatus;
+import hexlet.code.model.User;
+import hexlet.code.repository.TaskStatusRepository;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Mapper(
+    uses = {JsonNullableMapper.class, ReferenceMapper.class},
+    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+    componentModel = MappingConstants.ComponentModel.SPRING,
+    unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class TaskMapper {
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Mapping(target = "assignee", source = "assigneeId", qualifiedByName = "assigneeIdToUser")
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "description", source = "content")
+    @Mapping(target = "taskStatus", source = "status", qualifiedByName = "slugToTaskStatus")
+    public abstract Task map(TaskCreateDTO dto);
+
+    @Mapping(target = "assigneeId", source = "assignee.id")
+    @Mapping(target = "title", source = "name")
+    @Mapping(target = "content", source = "description")
+    @Mapping(target = "status", source = "taskStatus.slug")
+    public abstract TaskDTO map(Task model);
+
+    @Mapping(target = "assignee", source = "assigneeId", qualifiedByName = "assigneeIdToUser")
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "description", source = "content")
+    @Mapping(target = "taskStatus", source = "status", qualifiedByName = "slugToTaskStatus")
+    public abstract void update(TaskUpdateDTO dto, @MappingTarget Task model);
+
+    @Named("slugToTaskStatus")
+    public TaskStatus slugToTaskStatus(String slug) {
+        return taskStatusRepository.findBySlug(slug)
+            .orElseThrow(() -> new ResourceNotFoundException(
+                String.format("Task status with slug %s not found", slug)));
+    }
+
+    @Named("assigneeIdToUser")
+    public User assigneeIdToUser(Long id) {
+
+        if (id == null) {
+            return null;
+        }
+
+        var user = new User();
+        user.setId(id);
+        return user;
+    }
+}
